8a60e701d323e70c048da86a29605ff34ce94407
Addarsh Chandrasekar <addarsh@Addarshs-MacBook-Pro.local>
2019-08-22 23:54:46 -0700

    Fixed bash script to close backend on Ctrl-C

--- devservers.sh
+++ devservers.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+trap 'lsof -ti:8000 | xargs kill; exit' INT
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000 &
+
 
 mkdir -p /Users/addarsh/virtualenvs/eb-necklace/necklace/static
 cd ui
